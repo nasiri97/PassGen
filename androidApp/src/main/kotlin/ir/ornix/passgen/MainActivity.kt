f1c63e8516028899076cd8164a90c2c082077446
Hashem <nasiri97@yahoo.com>
2026-09-13 14:01:59 +0330

    Add logo to Android app

--- androidApp/src/main/kotlin/ir/ornix/passgen/MainActivity.kt
+++ androidApp/src/main/kotlin/ir/ornix/passgen/MainActivity.kt
@@ -1,17 +1,28 @@
 package ir.ornix.passgen
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import ir.ornix.passgen.composeapp.App
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import ir.ornix.passgen.composeapp.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+
+        /**
+         * Prevent Screenshots & Screen Recording (FLAG_SECURE)
+         */
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+
 
         setContent {
             App()
